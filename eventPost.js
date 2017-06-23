c55f9843243ae401eccce13d812576111c340140
Zachary Tang <zacharytang.tc@gmail.com>
2017-06-23 12:10:45 +0800

    standardised tab spacing

--- eventPost.js
+++ eventPost.js
@@ -2,13 +2,13 @@ var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:4000/myDatabase");
 
 var schema = new mongoose.Schema({
-  title: String,
-  content: String,
-  date: {
-    type: Date,
-    default: Date.now
-  },
-  category: String
+    title: String,
+    content: String,
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    category: String
 });
 
 module.exports = mongoose.model("EventPost", schema);
