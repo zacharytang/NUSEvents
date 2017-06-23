4feb86ff498b6df12f277cd3c627ad1b370b3930
Zachary Tang <zacharytang.tc@gmail.com>
2017-06-23 13:02:31 +0800

    merging branch

---
+++ eventPost.js
@@ -0,0 +1,18 @@
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:4000/myDatabase");
+
+var schema = new mongoose.Schema({
+    title: String,
+    content: String,
+    organiser: {
+        type: String,
+        default: "None"
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    category: String
+});
+
+module.exports = mongoose.model("EventPost", schema);
